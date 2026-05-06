dde22387e1f8b333449779654dce0e05af56752d
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-05-05 20:48:15 -0700

    feat: add ListBox and Select components with accessibility features using React Aria

---
+++ packages/ui/src/components2/ListBox.tsx
@@ -0,0 +1,102 @@
+import type { ComponentPropsWithRef } from "react";
+
+import { Check } from "lucide-react";
+import {
+  Collection as ListBoxCollection,
+  type CollectionProps as ListBoxCollectionProps,
+  Header as AriaHeader,
+  type HeaderProps as ListboxHeaderProps,
+  ListBox as AriaListBox,
+  ListBoxItem as AriaListBoxItem,
+  type ListBoxItemProps,
+  type ListBoxProps,
+  ListBoxSection,
+  type ListBoxSectionProps,
+} from "react-aria-components/ListBox";
+import { composeRenderProps } from "react-aria-components/composeRenderProps";
+import { cn, tv } from "tailwind-variants";
+
+import { composeTailwindRenderProps } from "../utils/composeTailwindRenderProps";
+import { focusRing } from "../utils/focusRing";
+
+const ListBox = <T extends object>({
+  className,
+  ...props
+}: ListBoxProps<T>) => {
+  return (
+    <AriaListBox
+      {...props}
+      className={composeTailwindRenderProps(
+        className,
+        "w-50 rounded-lg border border-border bg-surface p-1 font-sans outline-0",
+      )}
+    />
+  );
+};
+
+const listBoxItemVariants = tv({
+  extend: focusRing,
+  base: "group/list-box-item relative flex items-center gap-2 rounded-sm px-2.5 py-1.5 text-sm transition-[background-color,border-radius,opacity,color] forced-color-adjust-none select-none",
+  variants: {
+    isSelected: {
+      false:
+        "pressed:bg-border text-fg-bold -outline-offset-2 hover:bg-bg-muted",
+      true: [
+        // Outline-white! to override focusRing style
+        "bg-accent text-white -outline-offset-4 outline-white! forced-colors:bg-[Highlight] forced-colors:text-[HighlightText] forced-colors:outline-[HighlightText]",
+        // selectionMode="multiple
+        "[&+[data-selected]]:rounded-t-none [&:has(+[data-selected])]:rounded-b-none",
+      ],
+    },
+    isDisabled: {
+      true: "cursor-not-allowed text-fg-subtle forced-colors:text-[GrayText]",
+    },
+  },
+});
+
+const ListBoxItem = (props: ListBoxItemProps) => {
+  const textValue =
+    props.textValue ||
+    (typeof props.children === "string" ? props.children : undefined);
+
+  return (
+    <AriaListBoxItem
+      {...props}
+      textValue={textValue}
+      className={listBoxItemVariants}
+    >
+      {composeRenderProps(props.children, (children) => (
+        <>
+          <Check className="group-selected/list-box-item:visible group-selected/list-box-item:opacity-100 invisible size-4 opacity-0" />
+          {children}
+        </>
+      ))}
+    </AriaListBoxItem>
+  );
+};
+
+const ListBoxHeader = ({
+  className,
+  ...props
+}: ComponentPropsWithRef<typeof AriaHeader>) => {
+  return (
+    <AriaHeader
+      className={cn("py-1.5 pr-2 pl-8 text-sm font-semibold", className)}
+      {...props}
+    />
+  );
+};
+
+export {
+  ListBox,
+  type ListBoxProps,
+  ListBoxItem,
+  type ListBoxItemProps,
+  listBoxItemVariants,
+  ListBoxHeader,
+  type ListboxHeaderProps,
+  ListBoxSection,
+  type ListBoxSectionProps,
+  ListBoxCollection,
+  type ListBoxCollectionProps,
+};
---
+++ packages/ui/src/components2/Select.tsx
@@ -0,0 +1,112 @@
+import { ChevronDown } from "lucide-react";
+import {
+  Select as AriaSelect,
+  type SelectProps as AriaSelectProps,
+  Button,
+  ListBox,
+  type ListBoxProps,
+  type ListBoxItemProps as SelectItemProps,
+  SelectValue,
+  type ValidationResult,
+} from "react-aria-components/Select";
+import { cn, tv } from "tailwind-variants";
+
+import {
+  ListBoxItem as SelectItem,
+  ListBoxSection as SelectSection,
+  type ListBoxSectionProps as SelectSectionProps,
+} from "./ListBox";
+import { Popover } from "./Popover";
+import { Description, FieldError, Label } from "./form";
+import { composeTailwindRenderProps } from "../utils/composeTailwindRenderProps";
+import { focusRing } from "../utils/focusRing";
+
+const selectVariants = tv({
+  extend: focusRing,
+  base: [
+    "flex min-h-9 w-full min-w-max items-center justify-between gap-1 rounded-sm border border-border bg-surface px-2.5 pr-2 text-start text-sm/5 transition-colors",
+    "[-webkit-tap-highlight-color:transparent]",
+  ],
+  variants: {
+    isDisabled: {
+      false: [
+        "pressed:border-fg-muted text-fg-bold group-invalid/select:outline group-invalid/select:outline-destructive hover:border-fg-subtle",
+        "forced-colors:group-invalid/select:outline-[Mark]",
+      ],
+      true: [
+        "border-transparent bg-bg-muted text-neutral",
+        "forced-colors:text-[GrayText]",
+      ],
+    },
+  },
+});
+
+type SelectionMode = "single" | "multiple";
+
+type SelectProps<T extends object, M extends SelectionMode> = {
+  children: ListBoxProps<T>["children"];
+  label?: string;
+  description?: string;
+  errorMessage?: string | ((validation: ValidationResult) => string);
+  items?: Iterable<T>;
+} & Omit<AriaSelectProps<T, M>, "children">;
+
+const Select = <
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type -- Match `AriaSelectProps` type
+  T extends object = {},
+  M extends SelectionMode = "single",
+>({
+  className,
+  children,
+  label,
+  description,
+  errorMessage,
+  items,
+  ...props
+}: SelectProps<T, M>) => {
+  return (
+    <AriaSelect
+      {...props}
+      className={composeTailwindRenderProps(
+        className,
+        "group/select relative flex flex-col gap-1",
+      )}
+    >
+      {label && <Label>{label}</Label>}
+      <Button className={selectVariants}>
+        <SelectValue className="basis-content line-clamp-1 shrink-0 text-sm">
+          {({ selectedText, defaultChildren }) =>
+            selectedText || defaultChildren
+          }
+        </SelectValue>
+        <ChevronDown
+          aria-hidden
+          className={cn(
+            "size-4 shrink-0 text-fg-muted group-disabled/select:text-border",
+            "forced-colors:text-[ButtonText] forced-colors:group-disabled/select:text-[GrayText]",
+          )}
+        />
+      </Button>
+      {description && <Description>{description}</Description>}
+      <FieldError>{errorMessage}</FieldError>
+      <Popover containerPadding={0}>
+        <ListBox
+          items={items}
+          className="max-h-[inherit] overflow-auto p-1 outline-hidden"
+        >
+          {children}
+        </ListBox>
+      </Popover>
+    </AriaSelect>
+  );
+};
+
+export {
+  Select,
+  type SelectProps,
+  selectVariants,
+  SelectItem,
+  type SelectItemProps,
+  SelectSection,
+  type SelectSectionProps,
+};
