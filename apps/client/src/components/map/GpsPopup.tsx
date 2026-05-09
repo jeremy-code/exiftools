e5b72a3cd27ab81c749a3c81d80d69aac6aa0456
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-05-08 20:50:55 -0700

    feat: improve GpsPopup layout and default label handling

--- apps/client/src/components/map/GpsPopup.tsx
+++ apps/client/src/components/map/GpsPopup.tsx
@@ -16,7 +16,7 @@ import { Skeleton } from "@exifi/ui/components/Skeleton";
 const GpsPopupLabel = ({ coordinate }: { coordinate: LatLng }) => {
   const feature = useNominatimApiReverse(coordinate);
 
-  return feature?.properties?.display_name ?? null;
+  return feature?.properties?.display_name ?? "Unknown location";
 };
 
 type GpsPopupProps = {
@@ -27,47 +27,34 @@ type GpsPopupProps = {
 const GpsPopup = ({ coordinate, label, ...props }: GpsPopupProps) => {
   return (
     <Popup {...props}>
-      <div className="flex flex-col gap-1">
+      <div className="grid grid-cols-[auto_1fr] gap-2">
+        <MapPin className="size-4 text-muted-foreground" />
+        {label ?? (
+          <Suspense fallback={<Skeleton className="h-[1em] w-full" />}>
+            <GpsPopupLabel coordinate={coordinate} />
+          </Suspense>
+        )}
+
+        <LocateFixed className="size-4 text-muted-foreground" />
+        <div>{`${formatLatLng(coordinate)}`}</div>
+
+        <Link className="size-4 text-muted-foreground" />
         <div>
-          <div className="flex gap-2">
-            <div className="shrink-0 p-px">
-              <MapPin className="size-4" />
-            </div>
-            {label !== undefined ?
-              label
-            : <Suspense fallback={<Skeleton className="h-[1em] w-full" />}>
-                <GpsPopupLabel coordinate={coordinate} />
-              </Suspense>
-            }
-          </div>
-        </div>
-        <div className="flex gap-2">
-          <div className="shrink-0 p-px">
-            <LocateFixed className="size-4" />
-          </div>
-          <div>{`${formatLatLng(coordinate)} `}</div>
-        </div>
-        <div className="flex gap-2">
-          <div className="shrink-0 p-px">
-            <Link className="size-4" />
-          </div>
-          <div>
-            <HorizontalList>
-              <HorizontalListItem>
-                <a
-                  href={`https://www.openstreetmap.org/#map=18/${coordinate.lat}/${coordinate.lng}`}
-                  target="_blank"
-                >
-                  OpenStreetMap
-                </a>
-              </HorizontalListItem>
-              <HorizontalListItem>
-                <a href={formatLatLngAsGeoUri(coordinate)} target="_blank">
-                  geo URI
-                </a>
-              </HorizontalListItem>
-            </HorizontalList>
-          </div>
+          <HorizontalList>
+            <HorizontalListItem>
+              <a
+                href={`https://www.openstreetmap.org/#map=18/${coordinate.lat}/${coordinate.lng}`}
+                target="_blank"
+              >
+                OpenStreetMap
+              </a>
+            </HorizontalListItem>
+            <HorizontalListItem>
+              <a href={formatLatLngAsGeoUri(coordinate)} target="_blank">
+                geo URI
+              </a>
+            </HorizontalListItem>
+          </HorizontalList>
         </div>
       </div>
     </Popup>
