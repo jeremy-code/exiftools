66bbe90e0aa21516072e2d060e0909a6dd99c51d
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-05-12 22:27:21 -0700

    refactor: streamline getCurrentPosition function by consolidating promise logic

--- apps/client/src/utils/getCurrentPosition.ts
+++ apps/client/src/utils/getCurrentPosition.ts
@@ -1,13 +1,11 @@
 const getCurrentPosition = (options?: PositionOptions) => {
-  if ("geolocation" in navigator) {
-    return new Promise<GeolocationPosition>((resolve, reject) => {
+  return new Promise<GeolocationPosition>((resolve, reject) => {
+    if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(resolve, reject, options);
-    });
-  } else {
-    return new Promise<GeolocationPosition>((_resolve, reject) => {
+    } else {
       reject(new Error("Geolocation API is not supported in this browser"));
-    });
-  }
+    }
+  });
 };
 
 export { getCurrentPosition };
