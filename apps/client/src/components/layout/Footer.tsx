17c121920a8636ba52a2eb22000f7f87bf75ef9a
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-05-12 22:07:59 -0700

    feat: add Github icon component and update Footer to use it

--- apps/client/src/components/layout/Footer.tsx
+++ apps/client/src/components/layout/Footer.tsx
@@ -1,15 +1,12 @@
 import type { ComponentPropsWithRef } from "react";
 
-import { Code } from "lucide-react";
 import { cn } from "tailwind-variants";
 
 import { Button } from "@exifi/ui/components/Button";
 import { Link } from "@exifi/ui/components/Link";
+import { Github } from "@exifi/ui/components/icons/Github";
 
-export const Footer = ({
-  className,
-  ...props
-}: ComponentPropsWithRef<"footer">) => {
+const Footer = ({ className, ...props }: ComponentPropsWithRef<"footer">) => {
   return (
     <footer className={cn("shrink border-t", className)} {...props}>
       <div className="container flex items-center justify-between py-4">
@@ -26,11 +23,13 @@ export const Footer = ({
         </span>
         <Button asChild>
           <Link href="https://github.com/jeremy-code/exifi">
-            <Code />
-            Source code
+            <Github aria-hidden className="size-4" />
+            GitHub
           </Link>
         </Button>
       </div>
     </footer>
   );
 };
+
+export { Footer };
