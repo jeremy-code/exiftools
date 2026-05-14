657ab8e2ade11b2c98aaa2c4d6b6fc312856ced2
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-05-13 19:38:15 -0700

    refactor: simplify time stamp parsing logic and improve readability

--- apps/client/src/features/exif-editor/editors/quick/resolvers/resolveTimeStamp.ts
+++ apps/client/src/features/exif-editor/editors/quick/resolvers/resolveTimeStamp.ts
@@ -1,42 +1,37 @@
 import { millisecondsInSecond } from "date-fns/constants";
 import { Decimal } from "decimal.js";
-import {
-  exifFormatGetSize,
-  mapRationalFromObject,
-  mapRationalToObject,
-} from "libexif-wasm";
+import { exifFormatGetSize, mapRationalFromObject } from "libexif-wasm";
 
 import { MAX_UINT32_VALUE } from "#lib/exif/constants";
-import { newTypedArrayInFormat } from "#lib/exif/newTypedArrayInFormat";
+import { mapRationalArray } from "#lib/exif/mapRationalArray";
 import { approximateRational } from "#lib/math/approximateRational";
 
 import type { QuickEditorResolver } from "../types";
 
 const parseTimeStampValue = (value: number[]) => {
-  const timeStampValue = mapRationalToObject(
-    newTypedArrayInFormat(value, "RATIONAL"),
-  );
+  const timeStampValue = mapRationalArray(value);
 
   if (timeStampValue.length !== 3) {
     throw new Error(
       `Unexpected number of inputs for tag TIME_STAMP, expected 3, got ${timeStampValue.length}`,
     );
   }
-  const [hours, minutes, seconds] = timeStampValue.map((rational) =>
-    Decimal(rational.numerator).div(rational.denominator),
-  );
-  if (hours === undefined || minutes === undefined || seconds === undefined) {
+  const [hour, minute, second] = timeStampValue;
+  if (hour === undefined || minute === undefined || second === undefined) {
     throw new Error(
       "Hours, minutes, and seconds are required for tag TIME_STAMP",
     );
   }
-  const milliseconds = seconds.mod(1).mul(millisecondsInSecond).toNumber();
+  const millisecond = new Decimal(second)
+    .mod(1)
+    .mul(millisecondsInSecond)
+    .toNumber();
 
   return Temporal.PlainTime.from({
-    hour: hours.toNumber(),
-    minute: minutes.toNumber(),
-    second: seconds.toNumber(),
-    millisecond: milliseconds,
+    hour,
+    minute,
+    second: Math.floor(second),
+    millisecond,
   });
 };
 const resolveTimeStamp: QuickEditorResolver = (
