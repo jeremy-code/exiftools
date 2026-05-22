73e76bb93dc6964cfa3ed47d7dae01f24ecbc881
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-05-21 17:45:40 -0700

    feat: Replace hash-wasm with @noble/hashes for file hashing and update dependencies

--- apps/client/src/hooks/useFileHash.tsx
+++ apps/client/src/hooks/useFileHash.tsx
@@ -1,6 +1,7 @@
 import { use, useMemo } from "react";
 
-import { sha256 } from "hash-wasm";
+import { sha256 } from "@noble/hashes/sha2.js";
+import { bytesToHex } from "@noble/hashes/utils.js";
 
 // Needed to ensure the promise is stably cached for the same file across
 // multiple calls to useFileHash
@@ -9,9 +10,19 @@ const fileHashPromiseCache = new WeakMap<File, Promise<string>>();
 const getFileHashPromise = (file: File) => {
   let fileHashPromise = fileHashPromiseCache.get(file);
   if (fileHashPromise === undefined) {
-    fileHashPromise = file
-      .arrayBuffer()
-      .then((arrayBuffer) => sha256(new Uint8Array(arrayBuffer)));
+    fileHashPromise = file.arrayBuffer().then(async (arrayBuffer) => {
+      /**
+       * Hashes function using native WebCrypto if available (not avaliable in
+       * non-secure contexts, mostly for development), otherwise uses sha256
+       * from @noble/hashes
+       */
+      const hash =
+        "subtle" in crypto && "digest" in crypto.subtle ?
+          new Uint8Array(await crypto.subtle.digest("SHA-256", arrayBuffer))
+        : sha256(new Uint8Array(arrayBuffer));
+
+      return bytesToHex(hash); // bytesToHex uses Uint8Array.toHex if avaliable
+    });
     fileHashPromiseCache.set(file, fileHashPromise);
   }
   return fileHashPromise;
