82080232aceae9a1766857b784c56a7729e55811
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-05-25 00:10:30 -0700

    chore: improve addFormSchema validation for tagEntry, value

--- apps/client/src/features/exif-editor/forms/addEntryForm.ts
+++ apps/client/src/features/exif-editor/forms/addEntryForm.ts
@@ -2,19 +2,28 @@ import { formOptions } from "@tanstack/react-form";
 import { exifTagTableCount } from "libexif-wasm";
 import { z } from "zod";
 
+import { MAX_INT32_VALUE, MAX_UINT32_VALUE } from "#lib/exif/constants";
 import { FormatSchema, IfdSchema, TagEntrySchema } from "#schemas/exif";
 
 const addFormSchema = z.strictObject({
   ifd: IfdSchema,
   tagEntry: TagEntrySchema.extend({
-    index: z.number().min(0).max(exifTagTableCount()),
+    index: z
+      .int()
+      .min(0)
+      .max(exifTagTableCount() - 2), // Excluding NULL terminator AND decrementing by 1 for indexing from 0
   }),
   format: FormatSchema,
-  value: z.array(z.number()),
+  value: z.array(
+    z
+      .int()
+      .min(-1 * (MAX_INT32_VALUE + 1))
+      .max(MAX_UINT32_VALUE),
+  ),
 });
 
-type AddFormSchema = z.infer<typeof addFormSchema>;
-type AddFieldValues = Partial<AddFormSchema> & Pick<AddFormSchema, "value">;
+type AddFieldValues = Partial<z.infer<typeof addFormSchema>> &
+  Pick<z.infer<typeof addFormSchema>, "value">;
 
 const DEFAULT_FORM_VALUES: AddFieldValues = {
   ifd: undefined,
--- apps/client/src/lib/exif/constants.ts
+++ apps/client/src/lib/exif/constants.ts
@@ -1,5 +1,7 @@
 import type { DataType } from "libexif-wasm";
 
+const MAX_INT32_VALUE = 0x7fffffff;
+
 /**
  * Maximum value of a 32-bit unsigned integer
  */
@@ -13,4 +15,4 @@ const DATA_TYPE_MAP: Record<DataType, string> = {
   UNKNOWN: "Unknown",
 };
 
-export { MAX_UINT32_VALUE, DATA_TYPE_MAP };
+export { MAX_INT32_VALUE, MAX_UINT32_VALUE, DATA_TYPE_MAP };
